82c4be74f10a5bea02a31c4bb514c1aeca465d43
amrithvenkat <110397957+amrithvenkat@users.noreply.github.com>
2022-09-12 14:21:31 +0200

    SW-1848 Update serial flash tool code (#2)
    Update the baud rate to match bootloader code

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func run() error {
 	} else {
 		options := tty.OpenOptions{
 			PortName:              port,
-			BaudRate:              921600,
+			BaudRate:              115200,
 			DataBits:              8,
 			StopBits:              1,
 			MinimumReadSize:       1,
